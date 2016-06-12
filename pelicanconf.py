d3f63a7cd0c4177578493436e5f7e718279a8958
robotmaxtron <max.whittingham@gmail.com>
2016-06-12 16:17:04 -0500

    New theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 AUTHOR = u'robotmaxtron'
 SITENAME = u'http://robotmaxtron.github.io'
 SITEURL = ''
-
+THEME = "/home/robotmaxtron/pelican-themes/clean-blog"
 PATH = 'content'
 
 TIMEZONE = 'America/Chicago'
@@ -19,14 +19,19 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+# Social Stuff
+GITHUB_URL = 'http://github.com/robotmaxtron'
+TWITTER_URL = 'http://twitter.com/robotmaxtron'
+FACEBOOK_URL = 'http://facebook.com/max.whittingham'
 
 DEFAULT_PAGINATION = False
 
